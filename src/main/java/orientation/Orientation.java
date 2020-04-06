ccb19bb27964e2b0e13b33a4443951cac6b2a6cf
axal25 <emevig@gmail.com>
2020-04-06 04:43:36 +0200

    a good beginning, far from over

--- src/main/java/orientation/Orientation.java
+++
@@ -1,5 +0,0 @@
-package orientation;
-
-public enum Orientation {
-    H, V
-}
--- src/main/java/parser/OutputComposer.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-import cutter.Solution;
-
-public class OutputComposer {
-    public static String getOutputString(Solution solution) {
-        return (solution == null) ? "solution == null" : solution.toString(0);
-    }
-}
